d73f335169d0fe7372bb6bb2057c2e84a471da88
d0rmie <91803849+d0rmie@users.noreply.github.com>
2021-11-12 18:36:59 -0500

    created human object

--- console.js
+++ console.js
@@ -1,3 +1,16 @@
 const example = () => console.log('This is an example of a console log');
-example();
 
+
+const human = {
+    name: 'Jim',
+    surname: 'Smith',
+    explain() {
+        return example();
+    },
+    age: 45,
+    get name() {
+        return this.name;
+    }
+    };
+
+console.log(human.name);
